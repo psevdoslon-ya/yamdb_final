00945c5773311a9d845dbc0a821f7ef1971aacd1
Артём Матёров <psevdoslon.ya@yandex.ru>
2023-02-14 14:44:57 +0300

    Homework. Third commit.

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import User, Comment, Review
+from .models import Comment, Review, User
 
 admin.site.register(User)
 admin.site.register(Comment)
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,7 +1,8 @@
-from django.contrib.auth.models import AbstractUser
-from django.core.validators import (MaxValueValidator, MinValueValidator)
-from django.db import models
 import hashlib
+
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
 
 from .validators import year_validator
 
